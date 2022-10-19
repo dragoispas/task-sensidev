dc15fedd838d7219cab654b08c3ad6f7f56c61ec
dragoispas <drago.ispas@gmail.com>
2022-10-19 15:13:36 +0300

    set up initial characters grid data population

--- src/modules/character-grid/CharacterGridPage.tsx
+++ src/modules/character-grid/CharacterGridPage.tsx
@@ -1,3 +1,25 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { retrieveAllCharacters } from "../../app/charactersSlice";
+import { AppDispatch, RootState } from "../../app/store";
+
 export const CharacterGridPage = () => {
-    return <div></div>;
+    const dispatch = useDispatch();
+    const { characters } = useSelector((state: RootState) => state.characters);
+    
+    useEffect(() => {
+        (dispatch as AppDispatch)(retrieveAllCharacters());
+    }, []);
+
+    return (
+        <div>
+            {
+                characters.map((character) => (
+                    <div>
+                        <h3>{character.name}</h3>
+                    </div>
+                ))
+            }
+        </div>
+    );
 }
