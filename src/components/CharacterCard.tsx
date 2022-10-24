a3f9574d7a4cbdd6776c59ae76dfaadc76ee70cf
dragoispas <drago.ispas@gmail.com>
2022-10-24 18:26:45 +0300

    implemented the character grid page & related components

---
+++ src/components/CharacterCard.tsx
@@ -0,0 +1,64 @@
+import styled from "@emotion/styled";
+import { useNavigate } from "react-router-dom";
+import { Stack } from "./common";
+
+const Card = styled.div`
+    height: 150px;
+    width: 250px;
+    background: #b3c1cc;
+    border-radius: 15px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 5px;
+    border: 1px solid grey;
+    margin: 5px;
+    padding: 0px 10px;
+    background-image: url("./card.jpg");
+    background-size: 265px 150px;
+    cursor: pointer;
+
+    &:hover{
+        transform: translate(0px, -3px);
+        transition-duration: 0.1s;
+        // box-shadow: 0 0px 10px limegreen;
+    }
+`;
+
+const CardImage = styled.img`
+    height: 100px;
+    width: 100px;
+    border: 2px solid black;
+`;
+
+const CardText = styled.div<{fontSize?:string}>`
+    width: 125px;
+    word-wrap: break-word;
+    font-size: ${props => props.fontSize};
+`;
+
+interface Props{
+    name: string;
+    status: string;
+    image: string;
+    id: number;
+}
+
+export const CharacterCard:React.FC<Props> = ({name, status, image, id}) => {
+    const navigate = useNavigate();
+
+    const handleClick = () => {
+        console.log(`/character/${id}`);
+        navigate(`/character/${id}`);
+    };
+
+    return (
+        <Card onClick={handleClick}>
+            <CardImage className="character_image" src={image} alt='.'/>
+            <Stack gap="10px">
+                <CardText className="character_name">{name}</CardText>
+                <CardText fontSize="14px" className="character_status">Status: {status}</CardText>
+            </Stack>
+        </Card>
+    )
+}
---
+++ src/pages/CharacterGridPage.tsx
@@ -0,0 +1,58 @@
+import styled from "@emotion/styled";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { retrieveAllCharacters, retrieveMoreCharacters } from "../app/charactersSlice";
+import { AppDispatch, RootState } from "../app/store";
+import { useDebounce } from "../app/debounce";
+import { CharacterCard } from "../components/CharacterCard";
+import { SearchBar } from "../components/SearchBar";
+import { Button, Stack } from "../components/common";
+
+const CharacterGrid = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: center;
+    max-width: 1560px;
+
+    margin: 20px auto;
+`;
+
+export const CharacterGridPage = () => {
+    const dispatch = useDispatch();
+    const { characters, page, maxPage } = useSelector((state: RootState) => state.characters);
+
+    const [searchInput, setSearchInput] = useState<string>('');
+    const [status, setStatus] = useState<string>('');
+
+    const debouncedSearchInput = useDebounce(searchInput, 300);
+    
+    useEffect(() => {
+        (dispatch as AppDispatch)(retrieveAllCharacters({ name: debouncedSearchInput, status: status }));
+    }, [debouncedSearchInput, status, dispatch]);
+
+    return (
+        <Stack justifyContent="center" alignItems="center">
+            <img style={{margin: '10px auto'}} src="images/Rick&Morty.png" alt='.'></img>
+            <SearchBar searchInput={searchInput} setSearchInput={setSearchInput} status={status} setStatus={setStatus}/>
+            <CharacterGrid>
+                {
+                    characters ? (
+                        characters.map((character) => (
+                            <CharacterCard key={character.id} id={character.id} name={character.name.toUpperCase()} status={character.status} image={character.image}/>
+                        ))
+                    ) : (
+                        <></>
+                    )
+                }
+            </CharacterGrid>
+            { 
+                page === maxPage || characters.length===0 ? (
+                    <></>
+                ) : (
+                    <Button onClick={() => {(dispatch as AppDispatch)(retrieveMoreCharacters({ name: debouncedSearchInput, status: status, page: page + 1 }));}}>LOAD MORE</Button>
+                )
+            }
+        </Stack>
+    );
+}
